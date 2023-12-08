6b1b9714dd600555225ee8a4e42318b7788e1125
Seth Caster <111807881+JimiLimbo@users.noreply.github.com>
2023-12-08 11:03:00 -0600

    version

--- finalapppart1/test/widget_test.dart
+++ finalapppart1/test/widget_test.dart
@@ -7,24 +7,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:finalapppart1/main.dart';
+import 'package:finalapppart1/main.dart'; // Ensure correct import
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(LoginPage());
+  testWidgets('Login page test', (WidgetTester tester) async {
+    // Build the LoginPage and trigger a frame.
+    await tester.pumpWidget(MaterialApp(home: LoginPage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the Username and Password fields are present.
+    expect(find.byType(TextField), findsNWidgets(2));
+    expect(find.text('Username'), findsOneWidget);
+    expect(find.text('Password'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the presence of the Log In button.
+    expect(find.widgetWithText(ElevatedButton, 'Log In'), findsOneWidget);
   });
 }
+
